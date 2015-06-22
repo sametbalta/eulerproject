5a9763992d937a1a1227da0c270cab3c70359543
Samet Balta <sbalta@raloto.kartaca.net>
2015-06-22 19:04:18 +0300

    Problem 6 solved

---
+++ src/page1/Problem6.java
@@ -0,0 +1,37 @@
+/**
+ * Sum square difference
+ * https://projecteuler.net/problem=6
+ */
+/* 
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+package page1;
+
+public class Problem6 {
+	
+	private static int RANGE = 100;
+	
+	public static void main(String[] args) {
+	
+		long sumOfSquares = 0;
+		long squareOfSum = 0;
+		
+		for(int i = 0; i <= RANGE; i++) {
+			sumOfSquares += i*i;
+			squareOfSum += i;
+		}
+		
+		long answer = squareOfSum*squareOfSum - sumOfSquares;
+		
+		System.out.println("Answer: " + answer);
+	}
+
+}
